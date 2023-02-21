018bc059e25e436aa88093f802fcb89f61475b53
madara-tribe <tsukuba.univer@gmail.com>
2023-02-21 11:26:16 +0900

    as parts

--- video_capture.py
+++ video_capture.py
@@ -1,11 +1,12 @@
 import cv2
+import numpy as np
 from ultrasound_Sensor import SensorDist
 import time
 
 def sensor_dist(sensor):
     distance = sensor.getDistance()
     time.sleep(0.1)
-    return distance
+    return np.round(distance, decimals=2)
 
 def run_camera(opt, hyp):
     sensor = SensorDist(hyp)
@@ -17,7 +18,7 @@ def run_camera(opt, hyp):
         if i%opt.per_frames==0:
             distance = sensor_dist(sensor)
             texts = 'Distance: {} [cm/s]'.format(str(distance))
-            cv2.putText(frame, texts, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, [225, 255, 255], thickness=2)
+            cv2.putText(frame, texts, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, [255, 0, 0], thickness=3)
             frame = cv2.resize(frame, (500, 500))
             cv2.imshow('camera', frame)
             #print("{:.0f}cm".format(distance))
